6658b57d9b33492223e48729ae1c03c475f12a51
rlmf82 <rafael.lucas@gmail.com>
2020-06-28 17:36:19 +0100

    ajuste na nomeclatura de pacotes.

--- src/test/java/market/priceFeed/MarketPriceApplication.java
+++ src/test/java/market/priceFeed/MarketPriceApplication.java
@@ -1,10 +1,10 @@
-package prontuario.bitsaude;
+package market.priceFeed;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BitsaudeProntuarioApplicationTests {
+class MarketPriceApplication {
 
 	@Test
 	void contextLoads() {
